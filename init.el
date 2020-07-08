7bda7b135c2c8f297cb24fdd38d399ca93e2e515
Łukasz Kaźmierczak <luki.pol@gmail.com>
2020-07-08 14:20:38 +0200

    sync

--- init.el
+++ init.el
@@ -28,9 +28,12 @@
  '(head-hint nil t)
  '(hydra-key-format-spec "[%s]")
  '(package-selected-packages
-   '(evil-smartparens smarparens ob-async dired zoom-window yasnippet yaml-mode which-key use-package treemacs-projectile treemacs-evil string-inflection smex smartparens scala-mode sbt-mode pyvenv python-mode pyenv-mode protobuf-mode perspective ox-reveal org-super-agenda org-ref org-re-reveal org-plus-contrib org-bullets olivetti modus-operandi-theme lsp-ui lsp-python-ms lsp-metals lsp-ivy keychain-environment jupyter ivy-posframe helpful git-timemachine general forge flycheck evil-surround evil-snipe evil-org evil-magit evil-easymotion evil-commentary evil-collection emojify elfeed-goodies doom-modeline dockerfile-mode dired-subtree csv-mode counsel-projectile company-lsp blacken bind-map auto-compile academic-phrases))
+   '(org-roam flycheck-haskell haskell-mode eval-expr ivy-avy auctex ob-ammonite evil-smartparens smarparens ob-async dired zoom-window yasnippet yaml-mode which-key use-package treemacs-projectile treemacs-evil string-inflection smex smartparens scala-mode sbt-mode pyvenv python-mode pyenv-mode protobuf-mode perspective ox-reveal org-super-agenda org-ref org-re-reveal org-plus-contrib org-bullets olivetti modus-operandi-theme lsp-ui lsp-python-ms lsp-metals lsp-ivy keychain-environment jupyter ivy-posframe helpful git-timemachine general forge flycheck evil-surround evil-snipe evil-org evil-magit evil-easymotion evil-commentary evil-collection emojify elfeed-goodies doom-modeline dockerfile-mode dired-subtree csv-mode counsel-projectile company-lsp blacken bind-map auto-compile academic-phrases))
  '(safe-local-variable-values
-   '((lsp-python-ms-extra-paths "/home/porcupine/.pyenv/versions/master/lib/python3.7/site-packages"))))
+   '((lsp-python-ms-extra-paths quote
+				("/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend" "/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend/src"))
+     (lsp-python-ms-extra-paths "/home/porcupine/.pyenv/versions/master/lib/python3.7/site-packages")))
+ '(zoom-window-mode-line-color "#4682B4"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
