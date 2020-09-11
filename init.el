f8a15cacf41e6a70963b2af1b93c9a5200edbdd8
Łukasz Kaźmierczak <luki.pol@gmail.com>
2020-09-11 14:44:34 +0200

    sync

--- init.el
+++ init.el
@@ -34,9 +34,14 @@
  '(org-agenda-files
    '("~/Dropbox/org/todo/private.org" "~/Dropbox/org/todo/studies.org" "~/Dropbox/org/todo/work.org"))
  '(package-selected-packages
-   '(treemacs-icons-dired org-roam-server org-fancy-priorities prettier-js prettier-emacs scratch clojurescript-mode clojure-mode ob-ipython language-detection jiralib2 request ox-jira quelpa-use-package quelpa ejira hide-mode-line modus-operandi synosaurus reason-mode org-roam flycheck-haskell haskell-mode eval-expr ivy-avy auctex ob-ammonite evil-smartparens smarparens ob-async dired zoom-window yasnippet yaml-mode which-key use-package treemacs-projectile treemacs-evil string-inflection smex smartparens scala-mode sbt-mode pyvenv python-mode pyenv-mode protobuf-mode perspective ox-reveal org-super-agenda org-ref org-re-reveal org-plus-contrib org-bullets olivetti modus-operandi-theme lsp-ui lsp-python-ms lsp-metals lsp-ivy keychain-environment jupyter ivy-posframe helpful git-timemachine general forge flycheck evil-surround evil-snipe evil-org evil-magit evil-easymotion evil-commentary evil-collection emojify elfeed-goodies doom-modeline dockerfile-mode dired-subtree csv-mode counsel-projectile company-lsp blacken bind-map auto-compile academic-phrases))
+   '(guess-language modus-vivendi-theme treemacs-icons-dired org-roam-server org-fancy-priorities prettier-js prettier-emacs scratch clojurescript-mode clojure-mode ob-ipython language-detection jiralib2 request ox-jira quelpa-use-package quelpa ejira hide-mode-line modus-operandi synosaurus reason-mode org-roam flycheck-haskell haskell-mode eval-expr ivy-avy auctex ob-ammonite evil-smartparens smarparens ob-async dired zoom-window yasnippet yaml-mode which-key use-package treemacs-projectile treemacs-evil string-inflection smex smartparens scala-mode sbt-mode pyvenv python-mode pyenv-mode protobuf-mode perspective ox-reveal org-super-agenda org-ref org-re-reveal org-plus-contrib org-bullets olivetti modus-operandi-theme lsp-ui lsp-python-ms lsp-metals lsp-ivy keychain-environment jupyter ivy-posframe helpful git-timemachine general forge flycheck evil-surround evil-snipe evil-org evil-magit evil-easymotion evil-commentary evil-collection emojify elfeed-goodies doom-modeline dockerfile-mode dired-subtree csv-mode counsel-projectile company-lsp blacken bind-map auto-compile academic-phrases))
  '(safe-local-variable-values
-   '((projectile-project-root-files-functions projectile-root-bottom-up)
+   '((eval progn
+	   (pyenv-mode-set "ai-intent-backend")
+	   (+pyvenv/set-to-pyenv))
+     (eval pyenv-mode-set "ai-intent-backend")
+     (pyenv-mode-set . "ai-intent-backend")
+     (projectile-project-root-files-functions projectile-root-bottom-up)
      (lsp-python-ms-extra-paths quote
 				("/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend" "/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend/src"))
      (lsp-python-ms-extra-paths "/home/porcupine/.pyenv/versions/master/lib/python3.7/site-packages")))
