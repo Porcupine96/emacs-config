72e27f7ec566c10e34283e8994f4600b75d75a11
Porcupine <luki.pol@gmail.com>
2020-09-27 23:25:59 +0200

    sync

--- init.el
+++ init.el
@@ -15,38 +15,15 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file "~/my-emacs/configuration.org")
-
-;; ------------------------------ GENERATED ------------------------------
+(org-babel-load-file "~/.emacs.d/configuration.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("a1e54c03c2fa33d4cacabd32fcdbab8a8f3c287d8e6565605817c61cf179c6c0" "cc63dade39a93f82519c9eef9cbf4ba7d4378a81f46250ccfa5a29119b96e253" "7f8b4cbf5d20b71daf43378c41471130412b8fcfc3380c371d9ebb5b27416ba5" default))
- '(elfeed-search-remain-on-entry t t)
- '(head-hint nil t)
- '(hydra-key-format-spec "[%s]")
- '(olivetti-body-width 90)
- '(olivetti-recall-visual-line-mode-entry-state t)
- '(org-agenda-files
-   '("~/Dropbox/org/todo/private.org" "~/Dropbox/org/todo/studies.org" "~/Dropbox/org/todo/work.org"))
  '(package-selected-packages
-   '(guess-language modus-vivendi-theme treemacs-icons-dired org-roam-server org-fancy-priorities prettier-js prettier-emacs scratch clojurescript-mode clojure-mode ob-ipython language-detection jiralib2 request ox-jira quelpa-use-package quelpa ejira hide-mode-line modus-operandi synosaurus reason-mode org-roam flycheck-haskell haskell-mode eval-expr ivy-avy auctex ob-ammonite evil-smartparens smarparens ob-async dired zoom-window yasnippet yaml-mode which-key use-package treemacs-projectile treemacs-evil string-inflection smex smartparens scala-mode sbt-mode pyvenv python-mode pyenv-mode protobuf-mode perspective ox-reveal org-super-agenda org-ref org-re-reveal org-plus-contrib org-bullets olivetti modus-operandi-theme lsp-ui lsp-python-ms lsp-metals lsp-ivy keychain-environment jupyter ivy-posframe helpful git-timemachine general forge flycheck evil-surround evil-snipe evil-org evil-magit evil-easymotion evil-commentary evil-collection emojify elfeed-goodies doom-modeline dockerfile-mode dired-subtree csv-mode counsel-projectile company-lsp blacken bind-map auto-compile academic-phrases))
- '(safe-local-variable-values
-   '((eval progn
-	   (pyenv-mode-set "ai-intent-backend")
-	   (+pyvenv/set-to-pyenv))
-     (eval pyenv-mode-set "ai-intent-backend")
-     (pyenv-mode-set . "ai-intent-backend")
-     (projectile-project-root-files-functions projectile-root-bottom-up)
-     (lsp-python-ms-extra-paths quote
-				("/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend" "/home/porcupine/codeheroes/chatbotize/monorepo/ai/ai-intent-backend/src"))
-     (lsp-python-ms-extra-paths "/home/porcupine/.pyenv/versions/master/lib/python3.7/site-packages")))
- '(window-divider-mode nil)
- '(zoom-window-mode-line-color "#4682B4"))
+   '(hide-mode-line olivetti synosaurus eval-expr string-inflection elfeed csv-mode evil-smartparens smartparens prettier-js reason-mode flycheck-haskell haskell-mode yaml-mode lsp-ivy lsp-metals lsp-ui protobuf-mode sbt-mode clojure-mode lsp-python-ms blacken pyvenv pyenv-mode python-mode dockerfile-mode dired-subtree emojify org-re-reveal ox-reveal keychain-environment helpful company zoom-window doom-modeline treemacs-icons-dired treemacs-projectile treemacs-evil treemacs git-timemachine forge evil-magit magit counsel-projectile projectile yasnippet guess-language org-fancy-priorities org-roam-server org-roam jupyter which-key use-package smex scratch perspective org-super-agenda org-ref org-plus-contrib org-bullets ob-ipython ob-ammonite modus-vivendi-theme modus-operandi-theme ivy-posframe general flycheck evil-surround evil-snipe evil-org evil-easymotion evil-commentary evil-collection counsel auto-compile academic-phrases)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
