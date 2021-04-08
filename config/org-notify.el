cac72f765518360f2d5e08b5f548347739dc2944
Porcupine <luki.pol@gmail.com>
2021-04-08 20:46:26 +0200

    sync

--- config/org-notify.el
+++ config/org-notify.el
@@ -13,6 +13,7 @@
 (defvar org-notify-check-interval 3)
 (defvar org-notify-key "notify")
 (defvar org-notify--timer nil)
+(defvar org-notify-log-path "~/.emacs.d/org-notify-log.el")
 
 (defun org-notify--notify-headlines ()
   (org-element-map (org-element-parse-buffer) 'headline
@@ -26,11 +27,39 @@
  	       (org-element-property :scheduled headline)
 	       (org-element-property :raw-value headline))))))))
 
+
 (defun org-notify--handle-headline (id scheduled title)
-  (notifications-notify
-   :title "Notification"
-   :body title)
-  )
+  (with-current-buffer (or (get-file-buffer org-notify-log-path)
+	             	   (find-file-noselect org-notify-log-path))
+
+    (let* ((timers (eval (car (read-from-string (buffer-string)))))
+	   (time (gethash id timers))
+	   (time-seconds (string-to-number (org-notify--timestamp-to-seconds scheduled)))
+	   (now (float-time (current-time))))
+
+      (print time-seconds)
+      (print now)
+
+      (if time
+	(progn
+	  (print "Found it."))
+	(progn
+	  (print "Not found.")
+
+          (puthash id now timers)
+	  (erase-buffer)
+	  (prin1 timers (current-buffer)))))))
+
+(org-notify--process-file (-first-item org-agenda-files))
+
+  ;; (notifications-notify
+  ;;  :title "Notification"
+  ;;  :body title))
+
+(defun org-notify--timestamp-to-seconds (timestamp)
+  (let ((org-display-custom-times t)
+	(org-time-stamp-custom-formats '("%s")))
+    (org-timestamp-translate timestamp 'start)))
 
 (defun org-notify--process-file (path)
   (with-temp-buffer
@@ -40,8 +69,6 @@
       (-each (-flatten-n 1 notify-headlines)
 	(lambda (h)
 	  (apply 'org-notify--handle-headline h))))))
-
-(org-notify--process-file (-first-item org-agenda-files))
 
 (defun org-notify--check ()
   (-each org-agenda-files 'org-notify--process-file))
