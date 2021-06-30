8ab6d807a0d3a21919eac93f08945891accb7c25
Łukasz Kaźmierczak <luki.pol@gmail.com>
2021-06-30 12:44:13 +0200

    sync

--- init.el
+++ init.el
@@ -24,6 +24,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("436a604a458968224d520c4f09dd63d5c7a1c9e8500b206405c59fd72e199a8b" "38479f06147afd39cb47fc7b566be9489ce27c6b3da63236b37c9c49021b3c20" default))
  '(helm-minibuffer-history-key "M-p")
  '(warning-suppress-log-types '((comp) (:warning))))
 (custom-set-faces
