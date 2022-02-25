4fc3717c245c5e42725f6fcfcb3a459ddbf914a8
Łukasz Kaźmierczak <luki.pol@gmail.com>
2022-02-25 12:31:07 +0100

    sync

--- custom-vars.el
+++ custom-vars.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-types '((comp) (comp))))
+ '(safe-local-variable-values
+   '((eval setq projectile-project-compilation-cmd "make" lsp-pyright-extra-paths
+	   ["/home/porcupine/work/monorepo/ai/ai-supervised-clustering-backend/" "/home/porcupine/work/monorepo/ai/ai-supervised-clustering-backend/generated/"])))
+ '(warning-suppress-types '((comp) (comp)))
+ '(zoom-window-mode-line-color "#4682B4"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
