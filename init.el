a516b30f706e91f8542cddcd13078f157eae6cd1
Łukasz Kaźmierczak <luki.pol@gmail.com>
2022-03-06 13:03:43 +0100

    sync

--- init.el
+++ init.el
@@ -1,20 +1,20 @@
 ;; Configure package.el to include MELPA.
 
-;; TODO: do I still need it?
-(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+
+(require 'ob-tangle) 
 
 (defun get-modify-time (path)
   (time-convert (file-attribute-modification-time (file-attributes path)) 'integer))
-
 
 (if (let* ((org-modify-time (get-modify-time "~/.emacs.default/configuration.org"))
 	   (el-modify-time (get-modify-time "~/.emacs.default/configuration.el")))
       (< el-modify-time org-modify-time))
     (progn
-      (require 'org)
       (org-babel-tangle-file "~/.emacs.default/configuration.org" "~/.emacs.default/configuration.el")))
 
 
 (load-file "~/.emacs.default/configuration.el")
 
-;; (org-babel-load-file "~/.emacs.default/configuration.org")
+
+;; TODO: do I still need it?
+;; (add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
