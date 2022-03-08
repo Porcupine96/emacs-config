6be71971754f82b1c00e99949e55184c63df561e
Łukasz Kaźmierczak <luki.pol@gmail.com>
2022-03-08 22:10:10 +0100

    sync

--- init.el
+++ init.el
@@ -1,7 +1,23 @@
 ;; Configure package.el to include MELPA.
 
 
-(require 'ob-tangle) 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package org
+  :straight t)
 
 (defun get-modify-time (path)
   (time-convert (file-attribute-modification-time (file-attributes path)) 'integer))
