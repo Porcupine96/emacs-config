49cdd412272e392c6607f7db18d85484b3769cf8
Łukasz Kaźmierczak <luki.pol@gmail.com>
2022-05-14 13:10:38 +0200

    sync

--- packages/pelfeed.el
+++ packages/pelfeed.el
@@ -4,36 +4,72 @@
 ;;
 ;;; Code:
 (require 's)
+(require 'async)
 (require 'elfeed)
 (require 'elfeed-show)
 (require 'org-roam)
+
+
+;; (async-start
+;;    ;; What to do in the child process
+;;    (lambda ()
+;;      (message "This is a test")
+;;      (sleep-for 3)
+;;      222)
+
+;;    ;; What to do when it finishes
+;;    (lambda (result)
+;;      (message "Async process done, result should be 222: %s" result))))
 
 (defvar p/paper-directory "~/Dropbox/papers/")
 
 (defun p/elfeed-roam-note (entry)
   (interactive (list (elfeed-search-selected :ignore-region)))
 
-  (org-roam-capture- :templates
-                         '(("n" "note" plain "%?" :if-new
-                            (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
-                            :immediate-finish t
-                            :unnarrowed t))
-                         :node (org-roam-node-create :title (elfeed-entry-title entry))
-                         :props '(:finalize find-file)))
+  (let* ((article-link (elfeed-entry-link entry))
+         (pdf-link (p/arxiv-pdf-link article-link))
+         (location (p/article-location (elfeed-entry-title entry)))
+	 (template (concat
+		    "#+title: ${title}\n"
+		    "#+filetags: paper\n"
+		    "#+file: " location "\n"
+		    "#+startup: content latexpreview\n")))
 
+    (async-start
+     (lambda () (url-copy-file pdf-link location t))
+     (lambda (result) (message "Fetching %s finished with: %s" pdf-link result)))
+
+    (org-roam-capture- :templates
+                       `(("n" "note" plain "%?" :if-new
+                          (file+head "%(format-time-string \"%Y-%m-%d--%H-%M-%SZ--${slug}.org\" (current-time) t)"
+				     ,template)
+                          :immediate-finish t
+                          :unnarrowed t))
+                       :node (org-roam-node-create :title (elfeed-entry-title entry))
+                       :props '(:finalize find-file))))
+
+
+(defun p/arxiv-pdf-link (article-link)
+  (ignore-errors
+    (let* ((match-idx (string-match "arxiv.org/abs/\\([0-9.]*\\)" article-link))
+           (matched-arxiv-number (match-string 1 article-link)))
+
+      (when matched-arxiv-number
+	(s-concat "https://arxiv.org/pdf/" matched-arxiv-number)))))
+
+
+(defun p/article-location (title)
+  (s-concat p/paper-directory (s-dashed-words title) ".pdf"))
 
 (defun p/fetch-arxiv-paper (entry)
   (interactive (list (elfeed-search-selected :ignore-region)))
 
-  (let* ((link (elfeed-entry-link entry))
-         (match-idx (string-match "arxiv.org/abs/\\([0-9.]*\\)" link))
-         (matched-arxiv-number (match-string 1 link)))
-    (when matched-arxiv-number
-      (let* ((url (s-concat "https://arxiv.org/pdf/" matched-arxiv-number))
-	     (readable-name (s-dashed-words (elfeed-entry-title entry)))
-	     (location (s-concat p/paper-directory readable-name ".pdf")))
+  (let* ((article-link (elfeed-entry-link entry))
+	 (pdf-link (p/arxiv-pdf-link article-link)))
+    (when pdf-link
+      (let ((location (p/article-location (elfeed-entry-title entry))))
 	(if (not (file-exists-p location))
-	    (url-copy-file url location t))))))
+	    (url-copy-file pdf-link location t))))))
 
 (defun p/arxiv-feed (cat)
   (s-concat
