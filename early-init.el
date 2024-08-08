db5cf29111f705caac5348e9cf49a5bf74af76d2
Łukasz Kaźmierczak <luki.pol@gmail.com>
2024-08-08 23:29:32 +0200

    sync

---
+++ early-init.el
@@ -0,0 +1,8 @@
+;;; early-init.el --- Early initialization. -*- lexical-binding: t; -*-
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(undecorated-round . t))
+
+
+(provide 'early-init)
+;;; early-init.el ends here
