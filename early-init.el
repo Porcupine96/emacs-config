45c669daefcdbd17801ccb92b528ea37f1662149
Łukasz Kaźmierczak <luki.pol@gmail.com>
2025-03-22 12:33:14 +0100

    sync

--- early-init.el
+++ early-init.el
@@ -1,6 +1,8 @@
 ;;; early-init.el --- Early initialization. -*- lexical-binding: t; -*-
 (setq package-enable-at-startup nil)
 
+(setenv "LSP_USE_PLISTS" "true")
+
 (add-to-list 'default-frame-alist '(fullscreen . fullboth))
 (add-to-list 'default-frame-alist '(undecorated-round . t))
 
